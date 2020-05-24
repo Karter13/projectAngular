911f162b364e10bc7d0b1a916b8f5592dade2e3a
Karter13 <KarMiH1975@mail.ru>
2020-05-24 18:48:10 +0300

    Feat(projectAng#16) created posts.service.ts added Post method create()

--- projectAngularSPA/src/environments/environment.prod.ts
+++ projectAngularSPA/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ import {Environment} from "./interface";
 export const environment: Environment = {
   production: true,
   apiKey: 'AIzaSyBgVgHffSuTUuE6n8Q4KL5n-Obz6WuBXrg',
+  fbDbUrl: 'https://my-blog-3a704.firebaseio.com',
 };
--- projectAngularSPA/src/environments/environment.ts
+++ projectAngularSPA/src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyBgVgHffSuTUuE6n8Q4KL5n-Obz6WuBXrg'
+  apiKey: 'AIzaSyBgVgHffSuTUuE6n8Q4KL5n-Obz6WuBXrg',
+  fbDbUrl: 'https://my-blog-3a704.firebaseio.com',
 };
 
 /*
