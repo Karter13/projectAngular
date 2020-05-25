bd0e112f88913f007e36849097123d46d8f1bb01
Karter13 <karmih1975@mail.ru>
2020-05-25 15:16:04 +0300

    Feat(projectAng#18): created getAll in posts.service.ts added getting posts to dashboard-page

--- projectAngularSPA/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ projectAngularSPA/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.sass']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[];
+  postsSubscription: Subscription;
+
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
+   this.postsSubscription = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  remove(id: string) {
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.postsSubscription) {
+      this.postsSubscription.unsubscribe();
+    }
   }
 
 }
--- projectAngularSPA/src/app/shared/posts.service.ts
+++ projectAngularSPA/src/app/shared/posts.service.ts
@@ -23,4 +23,22 @@ export class PostsService {
         }),
       );
   }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(
+        map((response: {[key: string]: any}) => {
+          console.log(response);
+          return Object
+            .keys(response)
+            .map(key =>({
+              ...response[key],
+              id: key,
+              date: new Date(response[key].date)
+            }))
+        })
+      )
+  }
+
+
 }
