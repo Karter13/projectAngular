eafb61bc6b6edca0193ae204774febfa41005d1d
Karter13 <karmih1975@mail.ru>
2020-05-29 14:10:48 +0300

    Feat(dev#5): added a button in main-layout.component to go to admin panel

--- projectAngularSPA/src/app/shared/components/main-layout/main-layout.component.ts
+++ projectAngularSPA/src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
@@ -7,9 +8,12 @@ import { Component, OnInit } from "@angular/core";
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+  connectAd() {
+    this.router.navigate(['/admin', 'dashboard']);
+  }
 }
